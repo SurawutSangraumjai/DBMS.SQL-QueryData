9003af104230bbc0ca79a9e14aef548bf43f7f57
SurawutSangraumjai <65040233126@udru.ac.th>
2024-11-18 15:47:11 +0700

    update

--- DBMS.SQL QueryData/Form1.Designer.cs
+++ DBMS.SQL QueryData/Form1.Designer.cs
@@ -29,22 +29,58 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // dataGridView1
             // 
             dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(44, 28);
+            dataGridView1.Location = new Point(12, 104);
             dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(723, 372);
+            dataGridView1.Size = new Size(776, 334);
             dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(94, 28);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "พนักงาน";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(246, 28);
+            button2.Name = "button2";
+            button2.Size = new Size(138, 23);
+            button2.TabIndex = 2;
+            button2.Text = "หมวดหมู่สินค้า";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(463, 28);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 3;
+            button3.Text = "สินค้า";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "DBMS.SQL QueryData";
@@ -56,5 +92,8 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
